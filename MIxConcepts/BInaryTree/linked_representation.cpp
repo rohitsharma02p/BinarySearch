425f024b3e361f8a707743bb28dfd32fbe248ef7
Rohit Sharma <rohits@radiansys.com>
2024-04-10 22:07:57 +0530

    workign on recursion problems

---
+++ MIxConcepts/BInaryTree/linked_representation.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+int main()
+{
+
+    return 0;
+}
--- Recursion/03_height_binary_tree.cpp
+++ Recursion/03_height_binary_tree.cpp
@@ -2,15 +2,42 @@
 using namespace std;
 
 // Height of binary tree
-long long int factorial(int n)
-{
 
-}
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+// Function to calculate the height
+int height(Node *root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    int lh = height(root->left);
+    int rh = height(root->right);
+    return max(lh, rh) + 1;
+};
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << factorial(n);
+    // Height of tree = 3
+    // Constructing a simple binary tree:
+    //      1
+    //     / \
+    //    2   3
+    //       / \
+    //      4   5
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+    root->right->right = new Node(5);
+
+    // Call the height function and print the result
+    cout << "Height of the tree: " << height(root) << endl;
     return 0;
 }
